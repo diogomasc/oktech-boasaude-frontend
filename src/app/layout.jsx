b27947152f669196048813c2b890536933ebca2c
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2025-09-03 23:25:19 -0300

    fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,21 +3,15 @@ import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 
 export const metadata = {
-<<<<<<< HEAD
-  title: "My App",
-};
-=======
   title: "BOA SAUDE",
-  };
->>>>>>> e25856fb71936920191945d9ac843966bf9221d3
-
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <CartProvider>
-          <Header/>
+          <Header />
           {children}
         </CartProvider>
       </body>
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
-// app/page.jsx
-import { redirect } from 'next/navigation';
-=======
 import { LoginTemplate } from "@/template/LoginTemplate/loginTemplate";
+
 export const metadata = {
   title: "Login",
-  };
->>>>>>> e25856fb71936920191945d9ac843966bf9221d3
+};
 
-// Função fictícia para verificar se o usuário está logado
-// Aqui você pode substituir pela lógica real do seu sistema
-function isUserLoggedIn() {
-    // Exemplo: verificar token no localStorage ou cookies
-    if (typeof window !== "undefined") {
-        return !!localStorage.getItem("token"); // true se existir token
-    }
-    return false;
-}
-
-export default function Home() {
-    if (isUserLoggedIn()) {
-        redirect("/catalogo"); // se estiver logado, vai direto pro catálogo
-    } else {
-        redirect("/login"); // se não estiver logado, vai pro login
-    }
+export default function Page() {
+  return <LoginTemplate />;
 }
